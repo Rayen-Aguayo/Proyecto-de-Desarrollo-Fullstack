7e5f1bdd10221ed83e86c26a66acb1792a029ed1
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-04-22 19:32:08 -0400

    Add files via upload

---
+++ Consulta Medica y Odontologica/Consulta-Medica-y-Odontologica/src/main/java/com/example/Consulta/Medica/y/Odontologica/ConsultaMedicaYOdontologicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Consulta.Medica.y.Odontologica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaMedicaYOdontologicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaMedicaYOdontologicaApplication.class, args);
+	}
+
+}
---
+++ Consulta Medica y Odontologica/Consulta-Medica-y-Odontologica/src/test/java/com/example/Consulta/Medica/y/Odontologica/ConsultaMedicaYOdontologicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Consulta.Medica.y.Odontologica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaMedicaYOdontologicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
