d39d9ebd71681b901e4b095d662a1f0c342aa895
javiera-faundez <ja.faundezb@duocuc.cl>
2026-04-22 20:04:17 -0400

    feat: update model

---
+++ Consulta Medica y Odontologica/Consulta-Medica-y-Odontologica/src/main/java/com/example/Consulta/Medica/y/Odontologica/model/Paciente.java
@@ -0,0 +1,29 @@
+package com.example.Consulta.Medica.y.Odontologica.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "paciente")
+public class Paciente {
+    @Id
+    private String run;
+
+    private String datosDelPaciente;
+    private Integer edad;
+    private String alergias;
+    private String enfermedad;
+
+    private Boolean tomaMedicamento;
+    private String cualMedicameto;
+
+    private Integer nroTeléfono;
+
+}
