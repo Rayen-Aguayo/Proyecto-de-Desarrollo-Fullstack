471c48ef56065e1a21aab6e0805195da5075c081
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-04-22 20:44:24 -0400

    update"model"

--- Consulta Medica y Odontologica/Consulta-Medica-y-Odontologica/src/main/java/com/example/Consulta/Medica/y/Odontologica/model/Medico.java
+++ Consulta Medica y Odontologica/Consulta-Medica-y-Odontologica/src/main/java/com/example/Consulta/Medica/y/Odontologica/model/Medico.java
@@ -1,5 +1,21 @@
 package com.example.Consulta.Medica.y.Odontologica.model;
 
-public class Medico {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "medico")
+public class Medico {
+    @Id
+    private Integer edad;
+    private String run;
+    private Integer telefono;
+    private String especialidad;
 }
