f37f26dadb3916e45219f3adb3a96951f9d2865e
javiera-faundez <ja.faundezb@duocuc.cl>
2026-04-24 18:33:52 -0400

    feat: update ms-medico,model

--- consulta medica y odontologica/ms-medico/ms-medico/src/main/java/com/example/ms_medico/model/Medico.java
+++ consulta medica y odontologica/ms-medico/ms-medico/src/main/java/com/example/ms_medico/model/Medico.java
@@ -1,5 +1,23 @@
 package com.example.ms_medico.model;
 
-public class Medico {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "medico")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Medico {
+    @Id
+    private String run;
+
+    private Integer edad;
+    private Integer telefono;
+    private String especialidad;
 }
