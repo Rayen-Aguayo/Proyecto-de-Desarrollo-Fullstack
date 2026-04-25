6dcb71360d6b51b042703d90a17e5597c3676427
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-04-25 17:41:23 -0400

    Delete Consulta Medica y Odontologica/Consulta-Medica-y-Odontologica directory

--- Consulta Medica y Odontologica/Consulta-Medica-y-Odontologica/src/main/java/com/example/Consulta/Medica/y/Odontologica/repository/MedicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Consulta.Medica.y.Odontologica.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Consulta.Medica.y.Odontologica.model.Medico;
-
-public interface MedicoRepository extends JpaRepository<Medico, String>{
-
-}
--- Consulta Medica y Odontologica/Consulta-Medica-y-Odontologica/src/main/java/com/example/Consulta/Medica/y/Odontologica/repository/PacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Consulta.Medica.y.Odontologica.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Consulta.Medica.y.Odontologica.model.Paciente;
-
-public interface PacienteRepository extends JpaRepository<Paciente, String>{
-
-}
