8bc8ec0b55a35ece6d8f628c3c513769c73e7f6c
javiera-faundez <ja.faundezb@duocuc.cl>
2026-04-26 22:59:41 -0400

    feat: update pedir_hora

---
+++ consulta medica y odontologica/ms-pedir_hora/ms-pedir_hora/src/main/java/com/example/ms_pedir_hora/MsPedirHoraApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_pedir_hora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPedirHoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPedirHoraApplication.class, args);
+	}
+
+}
---
+++ consulta medica y odontologica/ms-pedir_hora/ms-pedir_hora/src/main/java/com/example/ms_pedir_hora/model/PedirHora.java
@@ -0,0 +1,31 @@
+package com.example.ms_pedir_hora.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "pedir_hora")
+public class PedirHora {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    private Date fecha;
+    private Integer horaDeAtención;
+    private Integer nroPaciente;
+    private String rutPaciente;
+    private String nombreMédico;
+    private String atencion;
+
+}
