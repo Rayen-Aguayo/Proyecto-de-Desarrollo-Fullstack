343405470a9fa1a042b32dbd4522bedb8da449d8
javiera-faundez <ja.faundezb@duocuc.cl>
2026-04-26 21:16:56 -0400

    feat:update pedir_hora

---
+++ ms-pedir_hora/ms-pedir_hora/src/test/java/com/example/ms_pedir_hora/MsPedirHoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_pedir_hora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedirHoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
