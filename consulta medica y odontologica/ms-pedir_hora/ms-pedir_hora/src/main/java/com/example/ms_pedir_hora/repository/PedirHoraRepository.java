9ea05bb024c7318352292955ffb9f628264551f0
javiera-faundez <ja.faundezb@duocuc.cl>
2026-04-26 23:02:41 -0400

    feat: update repository

---
+++ consulta medica y odontologica/ms-pedir_hora/ms-pedir_hora/src/main/java/com/example/ms_pedir_hora/repository/PedirHoraRepository.java
@@ -0,0 +1,9 @@
+package com.example.ms_pedir_hora.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ms_pedir_hora.model.PedirHora;
+
+public interface PedirHoraRepository extends JpaRepository<PedirHora, Long>  {
+
+}
